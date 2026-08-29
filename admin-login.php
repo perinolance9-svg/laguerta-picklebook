0330400875437d1457f9af0b83fa9b111ea0b5b8
perinolance <perinolance9@gmail.com>
2026-08-30 00:48:16 +0800

    Add separate admin and player login links

---
+++ admin-login.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+
+$loginPortal = 'admin';
+require __DIR__ . '/login.php';
---
+++ player-login.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+
+$loginPortal = 'player';
+require __DIR__ . '/login.php';
