5b9745f5321bd6a06609c2feb0c4007625e38fad
perinolance <perinolance9@gmail.com>
2026-08-30 11:32:03 +0800

    Refresh cached player dashboard design

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='laguerta-shell-v1';
+const CACHE='laguerta-shell-v2';
 const SHELL=['./assets/style.css','./assets/app-icon.svg','./offline.html'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
